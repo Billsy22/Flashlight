512e589b3f90b88e6eb852848493e8e07e91963a
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-08-05 20:00:09 -0600

    Filled in the action for the button in MainViewController

--- Flashlight/ViewControllers/MainViewController.swift
+++ Flashlight/ViewControllers/MainViewController.swift
@@ -15,11 +15,24 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // MARK: -  Properties
+    var isOn : Bool = false
+    
     // MARK: -  Outlets
     @IBOutlet weak var toggleButton: UIButton!
 
     // MARK: -  Actions
     @IBAction func toggleButtonTapped(_ sender: Any) {
-        print("Toggle Button Tapped Current State is ")
+        isOn = !isOn
+        if isOn == true {
+            toggleButton.titleLabel?.text = "Turn Off"
+            toggleButton.titleLabel?.textColor = UIColor.black
+            self.view.backgroundColor = UIColor.white
+        } else {
+            toggleButton.titleLabel?.text = "Turn On"
+            toggleButton.titleLabel?.textColor = UIColor.white
+            self.view.backgroundColor = UIColor.black
+        }
+        print("Toggle Button Tapped Current State is \(isOn)")
     }
 }
